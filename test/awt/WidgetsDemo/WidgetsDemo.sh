bf5300c5d9d834963e59f5ab090f1cf2f6f304c3
skumar <skumar>
1998-10-13 18:42:57 +0000

    Some changes

--- test/awt/WidgetsDemo/WidgetsDemo.sh
+++ test/awt/WidgetsDemo/WidgetsDemo.sh
@@ -1,2 +1,8 @@
-#! /bin/sh
-kaffe WidgetsDemo
+#!/bin/bash -x
+
+rm -f *.class
+
+Kaffe.jit pizza.compiler.Main DemoImageDict.java
+Kaffe.jit pizza.compiler.Main WidgetsDemo.java
+
+Kaffe.jit WidgetsDemo
