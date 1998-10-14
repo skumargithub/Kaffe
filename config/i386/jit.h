61938c8c16ac5f5414124a73074dcd8375a2da74
skumar <skumar>
1998-10-14 03:01:26 +0000

    Stuff about trampolines is now in trampolines.h

--- config/i386/jit.h
+++ config/i386/jit.h
@@ -51,32 +51,6 @@ typedef struct _exceptionFrame {
 		jmp *%1							\n\
 		" : : "g" (frame->retbp), "r" (info.handler), "g" (obj) : "eax")
 
-
-/**/
-/* Method dispatch.  */
-/**/
-
-#define HAVE_TRAMPOLINE
-
-typedef struct _methodTrampoline {
-	unsigned char call	__attribute__((packed));
-	int fixup		__attribute__((packed));
-	struct _methods* meth	__attribute__((packed));
-} methodTrampoline;
-
-extern void i386_do_fixup_trampoline(void);
-
-#define FILL_IN_TRAMPOLINE(t,m)						\
-	do {								\
-		(t)->call = 0xe8;					\
-		(t)->fixup = (int)i386_do_fixup_trampoline - (int)(t) - 5; \
-		(t)->meth = (m);					\
-	} while (0)
-
-#define FIXUP_TRAMPOLINE_DECL	Method** _pmeth
-#define FIXUP_TRAMPOLINE_INIT	(meth = *_pmeth)
-
-
 /**/
 /* Register management information. */
 /**/
