646461e315d6b716f6141cfaee928fbb6b7c3fcb
skumar <skumar>
1998-10-14 19:42:08 +0000

    Macros from stackTrace.h are here

--- kaffe/kaffevm/stackTrace.c
+++ kaffe/kaffevm/stackTrace.c
@@ -33,6 +33,40 @@
 #include "md.h"
 #include "stackTrace.h"
 
+#if defined(INTERPRETER)
+
+typedef struct _stackTrace {
+	vmException* frame;
+} stackTrace;
+
+#define STACKTRACEINIT(S,I,O)	((S).frame = (vmException*)unhand((*Kaffe_ThreadInterface.currentJava)())->exceptPtr)
+#define	STACKTRACESTEP(S)	((S).frame = (*Kaffe_ThreadInterface.nextFrame)((S).frame))
+#define STACKTRACEPC(S)		((S).frame->pc)
+#define STACKTRACEMETH(S)	((S).frame->meth)
+#define STACKTRACEEND(S)	((S).frame == 0 || (S).frame->meth == (Method*)1)
+
+#elif defined(TRANSLATOR)
+
+typedef struct _stackTrace {
+	struct _exceptionFrame	nframe;
+	struct _exceptionFrame* frame;
+} stackTrace;
+
+#define STACKTRACEINIT(S, I, O)				\
+	{						\
+		if ((I) == NULL) {			\
+			FIRSTFRAME((S).nframe, O);	\
+			(S).frame = &((S).nframe);	\
+		} else {				\
+			(S).frame = (I);		\
+		}					\
+	}
+#define	STACKTRACESTEP(S)	((S).frame = (*Kaffe_ThreadInterface.nextFrame)((S).frame))
+#define STACKTRACEPC(S)		(PCFRAME((S).frame))
+#define	STACKTRACEMETH(S)	(findMethodFromPC(PCFRAME((S).frame)))
+#define	STACKTRACEEND(S)	((S).frame == 0)
+
+#endif
 
 Hjava_lang_Object*
 buildStackTrace(struct _exceptionFrame* base)
